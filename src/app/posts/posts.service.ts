be60b6ac52cbd9d4df0d1ff457ef69f313565dc1
cajoue <cajoue@users.noreply.github.com>
2017-01-11 16:10:12 +0100

    feat: Create posts service
    to handle GET the Post data from the WordPress API

---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Post } from './post';
+
+@Injectable()
+export class PostsService {
+
+  //private postsUrl = "http://{YOUR_SITE_HERE}.com/wp-json/wp/v2/";
+  private postsUrl = "http://localhost:81/wp_angular/wp-json/wp/v2";
+
+  constructor(private http: Http) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http
+    .get(this.postsUrl + 'posts')
+    .map((res: Response) => res.json());
+  }
+}
